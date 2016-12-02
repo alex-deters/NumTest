07bed4d42379f8e9b77f9dc4563d27a8453014eb
Alex Deters <remade2.0@gmail.com>
2016-12-02 13:38:55 -0600

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+int main(){
+	bool break = false;
+	char chartempinput
+	int inttempinput;
+	vector<int>
+	while(!break){
+		std::cout << "Choose one of the following\nl: full learning mode\nd: detecting mode\ns: save and quit\n";
+		std::cin >> chartempinput;
+		switch(chartempinput){
+			case l:	
+				break;
+			case d:
+				break;
+			case c:
+				break;
+		};
+	}
+}
